b50451218008ba6814aee4d0668db6d8f001c891
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-20 02:31:50 +0330

    Add files via upload

---
+++ Compare Two Values.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main()
+
+{
+	float a,b ;
+	cout<<"enter a: ";
+	cin>>a;
+	cout<<"enter b: ";
+	cin>>b;
+	
+	if (a>b) cout<<"a is bigger than b"<<endl;
+	else cout<<"b is bigger than a"<<endl;
+	
+	getch();
+	
+	
+	
+}
